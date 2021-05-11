87e77656d85987639d87ba01fc8598baa20bf2da
You know me <hiyouknowme1515@gmail.com>
2021-05-11 15:06:23 +0530

    redux extention error fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,14 +10,25 @@ const initialState = {}
 
 const middleware = [thunk]
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+);
+    
+
+
 //store create by the createStore method with the parameters
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware( ...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    enhancer
 )
 
 export default store;
